3e93085212bc2f575f9c9f31cec9f9f0e441a2ed
Vivek Joshi <vj69376@gmail.com>
2025-09-26 23:56:44 +0530

    Inital commit

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,14 +2,25 @@ import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 function Navbar() {
+  const user = {}
   return (
-   <header className='header'>
+   <header className='navbar'>
     <nav>
         <Link href="/">
           <Image src="/assets/icons/logo.svg" alt="logo" width={32} height={32} />
           <h1>SnapCast</h1>
         </Link>
-    </nav>
+        {user &&( 
+          <figure>
+             <button>
+              <Image src="/assets/images/dummy.jpg" alt="user" width={36} height={36} className='rounded-full aspect-square' />
+             </button>
+             <button className='cursor-pointer'>
+
+             </button>
+          </figure>
+        )}
+    </nav> 
 
    </header>
   )
