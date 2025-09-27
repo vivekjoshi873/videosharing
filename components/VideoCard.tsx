c852b0290d140a109a52a6dda175a042edd09b64
Vivek Joshi <vj69376@gmail.com>
2025-09-27 13:10:04 +0530

    inital commit

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -1,11 +1,54 @@
-import React from 'react'
-
-function VideoCard() {
+import Image from "next/image";
+import React from "react";
+import Link from "next/link";
+function VideoCard({
+  id,
+  title,
+  thumbnail,
+  createdAt,
+  userImg,
+  username,
+  views,
+  visibility,
+  duration,
+}: VideoCardProps) {
   return (
-    <div>
-      
-    </div>
-  )
+    <Link href={`/video/${id}`} className="video-card">
+      <Image
+        src={thumbnail}
+        alt="thumbnail"
+        width={290}
+        height={160}
+        className="thumbnail"
+      />
+      <article>
+        <div>
+          <figure>
+            <Image
+              src={userImg}
+              alt="avatar"
+              width={34}
+              height={34}
+              className="rounded-full aspect-square"
+            />
+            <figcaption>
+              <h3>{username}</h3>
+              <p>{visibility}</p>
+            </figcaption>
+          </figure>
+          <aside>
+            <Image
+              src="/assets/icons/eye.svg"
+              alt="views"
+              width={16}
+              height={16}
+            />  
+            <span>{views}</span>
+          </aside>
+        </div>
+      </article>
+    </Link>
+  );
 }
 
-export default VideoCard
+export default VideoCard;
--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -4,12 +4,19 @@ import VideoCard from "../../../components/VideoCard";
 const Page = () => {
   return (
     <main className="wrapper page">
-      <Header
-        subHeader="All Video"
-        title="Public Library"
+      <Header subHeader="All Video" title="Public Library" />
+      <h1 className="text-3xl">Welcome to loom clone</h1>
+      <VideoCard
+        id="1"
+        title="Sample Video"
+        thumbnail="/assets/samples/thumbnail (1).png"
+        createdAt="2025-05-01 06:25:54:437"
+        userImg="/assets/images/jason.png"
+        username="Jason"
+        views={23}
+        visibility="public"
+        duration={156}
       />
-      <h1>Welcome to loom clone</h1>
-      <VideoCard/>
     </main>
   );
 };
