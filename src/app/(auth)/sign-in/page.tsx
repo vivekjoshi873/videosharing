8c80a3c9d11850f8a1e8e077e47fd421405e3124
Vivek Joshi <vj69376@gmail.com>
2025-09-28 22:13:24 +0530

    make the auth page of signin page

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,11 +1,75 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 function page() {
   return (
-    <div>
-      Auth
-    </div>
-  )
+    <main className="sign-in">
+      <aside className="testimonal">
+        <Link href="/">
+          <Image
+            src="/assets/icons/logo.svg"
+            alt="SnapCast Logo"
+            width={32}
+            height={32}
+          />
+          <h1>SnapCast</h1>
+        </Link>
+        <div className="description">
+          <section>
+            <figure>
+              {Array.from({ length: 5 }).map((_, index) => (
+                <Image
+                  src="/assets/icons/star.svg"
+                  alt="star"
+                  width={20}
+                  height={20}
+                  key={index}
+                />
+              ))}
+            </figure>
+            <p>
+              SnapCast makes screen recording easy. From quick walkthroughs to
+              full presentations, it's fast, smooth, and shareable in seconds
+            </p>
+            <article>
+              <Image
+                src="/assets/images/jason.png"
+                alt="jason"
+                width={64}
+                height={64}
+                className="rounded-full"
+              />
+              <div>
+                <h2>Jason Rivera</h2>
+                <p>Product Designer, NovaByte</p>
+              </div>
+            </article>
+          </section>
+        </div>
+        <p>© Snapcast {new Date().getFullYear()}</p>
+      </aside>
+      <aside className="google-sign-in">
+        <section>
+          <Link href="/">
+            <Image
+              src="/assets/icons/logo.svg"
+              alt="Google"
+              width={40}
+              height={40}
+            />
+            <h1>SnapCast</h1>
+          </Link>
+          <p>
+            Create and share your very <span>SnapCast video</span>in no time!
+          </p>
+          <button>
+            <Image />
+          </button>
+        </section>
+      </aside>
+    </main>
+  );
 }
 
-export default page
+export default page;
---
+++ src/app/(root)/video/[videoId]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <main className='wrapper page'>
+      Video Details Page
+    </main>
+  )
+}
+
+export default page
