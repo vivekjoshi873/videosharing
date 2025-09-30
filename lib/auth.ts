e7f54701230f3f94de69999d0683d60672d43ba4
Vivek Joshi <vj69376@gmail.com>
2025-09-30 14:53:51 +0530

    inital commit

---
+++ lib/auth.ts
@@ -0,0 +1,6 @@
+import {betterAuth} from 'better-auth'
+import { drizzleAdapter } from 'better-auth/adapters/drizzle'
+import { db } from '../drizzle/db'
+export const auth = betterAuth({
+    database:drizzleAdapter(db,{provider:"pg"})
+})
