179f2e280239224115f64a93466ac5fbd5c817d6
Vivek Joshi <vj69376@gmail.com>
2025-10-07 23:17:20 +0530

    Inital commit

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,8 +1,8 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 import { ilike, sql } from "drizzle-orm";
-import { videos } from "@/drizzle/schema";
-import { DEFAULT_VIDEO_CONFIG, DEFAULT_RECORDING_CONFIG } from "@/constants";
+// import { videos } from "../drizzle/schema";
+import { DEFAULT_VIDEO_CONFIG, DEFAULT_RECORDING_CONFIG } from "../constants";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -99,19 +99,19 @@ export const withErrorHandling = <T, A extends unknown[]>(
   };
 };
 
-export const getOrderByClause = (filter?: string) => {
-  switch (filter) {
-    case "Most Viewed":
-      return sql`${videos.views} DESC`;
-    case "Least Viewed":
-      return sql`${videos.views} ASC`;
-    case "Oldest First":
-      return sql`${videos.createdAt} ASC`;
-    case "Most Recent":
-    default:
-      return sql`${videos.createdAt} DESC`;
-  }
-};
+// export const getOrderByClause = (filter?: string) => {
+//   switch (filter) {
+//     case "Most Viewed":
+//       return sql`${videos.views} DESC`;
+//     case "Least Viewed":
+//       return sql`${videos.views} ASC`;
+//     case "Oldest First":
+//       return sql`${videos.createdAt} ASC`;
+//     case "Most Recent":
+//     default:
+//       return sql`${videos.createdAt} DESC`;
+//   }
+// };
 
 export const generatePagination = (currentPage: number, totalPages: number) => {
   if (totalPages <= 7) {
